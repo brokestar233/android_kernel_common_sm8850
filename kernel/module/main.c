518a659abeb7cda3408757219aff3f0d718365fe
brokestar233 <3765589194@qq.com>
2026-03-23 22:58:44 +0800

    modules: add custom_module_blacklist
    Signed-off-by: brokestar233 <3765589194@qq.com>

--- kernel/module/main.c
+++ kernel/module/main.c
@@ -2797,13 +2797,18 @@ int __weak module_frob_arch_sections(Elf_Ehdr *hdr,
 
 /* module_blacklist is a comma-separated list of module names */
 static char *module_blacklist;
+static char *custom_module_blacklist[] = {
+	"oplus_secure_guard_new"
+};
+
 static bool blacklisted(const char *module_name)
 {
 	const char *p;
 	size_t len;
+	int i;
 
 	if (!module_blacklist)
-		return false;
+		goto custom_blacklist;
 
 	for (p = module_blacklist; *p; p += len) {
 		len = strcspn(p, ",");
@@ -2812,6 +2817,11 @@ static bool blacklisted(const char *module_name)
 		if (p[len] == ',')
 			len++;
 	}
+
+custom_blacklist:
+	for (i = 0; i < ARRAY_SIZE(custom_module_blacklist); i++)
+		if (!strcmp(module_name, custom_module_blacklist[i]))
+			return true;
 	return false;
 }
 core_param(module_blacklist, module_blacklist, charp, 0400);
@@ -3274,10 +3284,6 @@ static int early_mod_check(struct load_info *info, int flags)
 	 * Now that we know we have the correct module name, check
 	 * if it's blacklisted.
 	 */
-	if (blacklisted(info->name)) {
-		pr_err("Module %s is blacklisted\n", info->name);
-		return -EPERM;
-	}
 
 	err = rewrite_section_headers(info, flags);
 	if (err)
@@ -3384,6 +3390,11 @@ skip_intercept:
 	err = early_mod_check(info, flags);
 	if (err)
 		goto free_copy;
+
+	if (blacklisted(info->name)) {
+		pr_err("Module %s is blacklisted\n", info->name);
+		goto free_copy;
+	}
 
 	/* Figure out module layout, and allocate all the memory. */
 	mod = layout_and_allocate(info, flags);
